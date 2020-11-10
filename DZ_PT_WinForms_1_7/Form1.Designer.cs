1bccb58e81589bb44c73b708942a6f00d02fae92
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-11-10 19:24:49 +0200

    1924

--- DZ_PT_WinForms_1_7/Form1.Designer.cs
+++ DZ_PT_WinForms_1_7/Form1.Designer.cs
@@ -41,27 +41,27 @@
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(88, 16);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(96, 7);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(136, 22);
+            this.textBox1.Size = new System.Drawing.Size(109, 26);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "До даты:";
             // 
             // radioButton_Year
             // 
             this.radioButton_Year.AutoSize = true;
-            this.radioButton_Year.Location = new System.Drawing.Point(24, 80);
+            this.radioButton_Year.Location = new System.Drawing.Point(16, 43);
             this.radioButton_Year.Name = "radioButton_Year";
             this.radioButton_Year.Size = new System.Drawing.Size(42, 17);
             this.radioButton_Year.TabIndex = 3;
@@ -73,17 +73,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(240, 24);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(211, 10);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.Size = new System.Drawing.Size(80, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "осталось";
             // 
             // radioButton_Month
             // 
             this.radioButton_Month.AutoSize = true;
-            this.radioButton_Month.Location = new System.Drawing.Point(88, 80);
+            this.radioButton_Month.Location = new System.Drawing.Point(79, 43);
             this.radioButton_Month.Name = "radioButton_Month";
             this.radioButton_Month.Size = new System.Drawing.Size(57, 17);
             this.radioButton_Month.TabIndex = 5;
@@ -94,7 +94,7 @@
             // radioButton_Day
             // 
             this.radioButton_Day.AutoSize = true;
-            this.radioButton_Day.Location = new System.Drawing.Point(160, 80);
+            this.radioButton_Day.Location = new System.Drawing.Point(156, 43);
             this.radioButton_Day.Name = "radioButton_Day";
             this.radioButton_Day.Size = new System.Drawing.Size(49, 17);
             this.radioButton_Day.TabIndex = 6;
@@ -105,7 +105,7 @@
             // radioButton_Minute
             // 
             this.radioButton_Minute.AutoSize = true;
-            this.radioButton_Minute.Location = new System.Drawing.Point(224, 80);
+            this.radioButton_Minute.Location = new System.Drawing.Point(227, 43);
             this.radioButton_Minute.Name = "radioButton_Minute";
             this.radioButton_Minute.Size = new System.Drawing.Size(55, 17);
             this.radioButton_Minute.TabIndex = 7;
@@ -116,7 +116,7 @@
             // radioButton_Second
             // 
             this.radioButton_Second.AutoSize = true;
-            this.radioButton_Second.Location = new System.Drawing.Point(296, 80);
+            this.radioButton_Second.Location = new System.Drawing.Point(296, 43);
             this.radioButton_Second.Name = "radioButton_Second";
             this.radioButton_Second.Size = new System.Drawing.Size(60, 17);
             this.radioButton_Second.TabIndex = 8;
@@ -127,10 +127,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(344, 40);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(297, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(15, 16);
+            this.label3.Size = new System.Drawing.Size(18, 20);
             this.label3.TabIndex = 9;
             this.label3.Text = "0";
             // 
@@ -138,7 +138,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(465, 122);
+            this.ClientSize = new System.Drawing.Size(416, 76);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.radioButton_Second);
             this.Controls.Add(this.radioButton_Minute);
@@ -148,7 +148,10 @@
             this.Controls.Add(this.radioButton_Year);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DZ_PT_WinForms_1_7";
             this.ResumeLayout(false);
--- DZ_PT_WinForms_1_7/Form1.cs
+++ DZ_PT_WinForms_1_7/Form1.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace DZ_PT_WinForms_1_7
 {
     public partial class Form1 : Form
@@ -15,53 +7,38 @@ namespace DZ_PT_WinForms_1_7
         public Form1()
         {
             InitializeComponent();
-
-
-
+            textBox1.Text = "YYYY/MM/DD";
+            label3.Text = "0";
+            radioButton_Year.Checked = true;
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             try
             {
-                DateTime dayT = new DateTime();
-                dayT = DateTime.Parse(textBox1.Text);
+                DateTime dayCalc = new DateTime();
+                dayCalc = DateTime.Parse(textBox1.Text);
                 DateTime dayNow = DateTime.Now;
-                TimeSpan tempDay = dayT - dayNow;
-                if (dayT > dayNow)
+                TimeSpan diffDay = dayCalc - dayNow;
+                if (dayCalc > dayNow)
                 {
                     if (radioButton_Year.Checked)
-                    {
-                        label3.Text = (tempDay.TotalDays / 365).ToString();
-                    }
+                        label3.Text = Math.Round(diffDay.TotalDays / 365.25, 1).ToString() + " лет";
                     if (radioButton_Month.Checked)
-                    {
-                        label3.Text = (tempDay.TotalDays / 30).ToString();
-                    }
+                        label3.Text = Math.Round(diffDay.TotalDays / 30, 1).ToString() + " месяцев";
                     if (radioButton_Day.Checked)
-                    {
-                        label3.Text = ((int)tempDay.TotalDays).ToString();
-                    }
+                        label3.Text = ((int)diffDay.TotalDays).ToString() + " дней";
                     if (radioButton_Minute.Checked)
-                    {
-                        label3.Text = ((int)tempDay.TotalMinutes).ToString();
-                    }
+                        label3.Text = ((int)diffDay.TotalMinutes).ToString() + " минут";
                     if (radioButton_Second.Checked)
-                    {
-                        label3.Text = ((int)tempDay.TotalSeconds).ToString();
-                    }
+                        label3.Text = ((int)diffDay.TotalSeconds).ToString() + " секунд";
                 }
                 else
-                {
-                    label3.Text = "Эта дата уже прошла!";
-                }
+                    label3.Text = "Прошлое...";
             }
             catch (Exception)
             {
-                label3.Text = " Неверный формат даты";
+                label3.Text = " Неформат...";
             }
-
-
         }
     }
 }
