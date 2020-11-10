eef2376f4986d18b8afc99d4224357da4ab2f623
chmilrv@gmail.com <chmilrv@gmail.com>
2020-11-10 15:21:23 +0200

    1521

---
+++ DZ_PT_WinForms_1_6/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ_PT_WinForms_1_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = TranslateDayOfWeek(dateTimePicker1.Value.DayOfWeek.ToString());
+        }
+
+        private string TranslateDayOfWeek(string day)
+        {
+            Dictionary<string, string> DayOfWeek = new Dictionary<string, string>
+            {
+                ["Monday"] = "Понедельник",
+                ["Tuesday"] = "Вторник",
+                ["Wednesday"] = "Среда",
+                ["Thursday"] = "Четверг",
+                ["Friday"] = "Пятница",
+                ["Saturday"] = "Суббота",
+                ["Sunday"] = "Воскресенье"
+            };
+            foreach (KeyValuePair<string, string> days in DayOfWeek)
+            {
+                if (days.Key == dateTimePicker1.Value.DayOfWeek.ToString())
+                    day = days.Value;
+            }
+            return day;
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = TranslateDayOfWeek(dateTimePicker1.Value.DayOfWeek.ToString());
+        }
+    }
+}
